f3361f3ad56f9dab68951f397a314da71b2dd195
Felipe Rodrigues <felipe.rod00@hotmail.com>
2025-02-02 17:54:42 -0300

    alter

--- src/controllers/TypeTransactionController.js
+++ src/controllers/TypeTransactionController.js
@@ -3,7 +3,7 @@ const TypeTransaction = require("../models/TypeTransaction.js")
 exports.getAllTypeTransaction = async (req, res) => {
     try {
       const typeTransaction  = await TypeTransaction.findAll();
-      res.status(200).json({ TextDecoderypeTransaction });
+      res.status(200).json({ typeTransaction });
     } catch (error) {
       res.status(500).json({ message: 'Error fetching TypeTransaction', error: error.message });
     }
